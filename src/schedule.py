ca51952e205b6d2d3be54be31df6e8de8a4282c7
Suraj Shanbhag <surajus@thoughtworks.com>
2017-08-23 14:36:58 +0530

    Suraj Shanbhag Connect Convert Date to Time

--- src/schedule.py
+++ src/schedule.py
@@ -1,11 +1,15 @@
+import datetime
+
+
 class Schedule(object):
     def __init__(self, cursor):
         self.cursor = cursor
 
     def get_schedule(self, parameters, limit):
-        self.cursor.execute(
-            "SELECT * FROM SCHEDULE WHERE start_code='%s' and end_code='%s' and day='%s' and time_of_arrival>='%s' order by time_of_arrival limit %s" \
-            % (parameters['source'], parameters['destination'], parameters['day'], parameters['time'], limit))
+        day = datetime.datetime.strptime(parameters['day'], '%Y-%m-%d').strftime('%a')
+        query = "SELECT * FROM SCHEDULE WHERE start_code='%s' and end_code='%s' and day='%s' and time_of_arrival>='%s' order by time_of_arrival limit %s" % (
+            parameters['source'], parameters['destination'], day, parameters['time'], limit)
+        self.cursor.execute(query)
         return self._dicnarify(self.cursor.fetchall())
 
     def _dicnarify(self, results):
