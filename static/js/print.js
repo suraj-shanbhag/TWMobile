ce9649caa24313317d75506113597551ed2326db
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-08-31 12:59:39 +0530

    Provide user with mail option to mail their booked ticket

--- static/js/print.js
+++ static/js/print.js
@@ -1,8 +1,16 @@
 function printDiv(divName) {
-     var printContents = document.getElementById(divName).innerHTML;
-     var originalContents = document.body.innerHTML;
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
 
-     document.body.innerHTML = printContents;
-     window.print();
-     document.body.innerHTML = originalContents;
+function mail(divName){
+    var printContents = document.getElementById(divName);
+    var originalContents = document.body.innerHTML;
+    var email = document.getElementsByTagName('input')['email'].value;
+    document.body.innerHTML = printContents;
+    window.location.href="mailto:" + email + "?subject="+document.title+"&body="+printContents.innerText;
+    document.body.innerHTML = originalContents;
 }
