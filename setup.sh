6d0c17c78dd42557bf1c6d09ceb0531138e1605c
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-09-07 15:51:04 +0530

    Add EMAIL_ID and EMAIL_PASSWORD in the setup.sh file to enable sending ticet through email feature

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 export DATABASE='YOUR DATABASE NAME'
 export USER='USER NAME'
 export HOST='localhost OR REMOTE LOCATION'
-export PORT='PORT'
+export PORT=5432
 export PASSWORD='PASSWORD IF ANY OR BLANK'
+export EMAIL_ID=yourEmail@gmail.com
+export EMAIL_PASSWORD=yourPassword
 echo "Cheers All the variables were set properly ;)"
